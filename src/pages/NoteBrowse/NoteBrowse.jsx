363c2cb3ea02e06adf78e35a757b5964c741b53c
Fl0-dev <florian@lamacompta.co>
2023-04-02 14:55:36 +0200

    Add : TextCard

--- src/pages/NoteBrowse/NoteBrowse.jsx
+++ src/pages/NoteBrowse/NoteBrowse.jsx
@@ -1,9 +1,16 @@
+import { TextCard } from 'components/TextCard/TextCard';
 import s from './NoteBrowse.module.css';
 
 export function NoteBrowse(props) {
     return (
-        <div className={s.NoteBrowse}>
-        <h1>NoteBrowse</h1>
-        </div>
+        <>
+            <TextCard
+                title="azeazeaz"
+                subtitle="01/01/22"
+                content=" akzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajek akzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajekakzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajekakzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajek akzejakzeja jaekajzekaje jaekjazkejazkejakejazejazkejakzejazkej jaekajekajek"
+                onClickTrash={() => alert("Click trash")}
+                onClick={() => alert("Click note")}
+            />
+        </>
     );
-    }
+}
