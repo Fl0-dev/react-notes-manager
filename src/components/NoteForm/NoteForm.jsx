960c9fa00897aa37652f88f061c0583908035d8d
Fl0-dev <florian@lamacompta.co>
2023-04-03 08:58:26 +0200

    Add : NoteForm + NoteCreate for  add new note

--- src/components/NoteForm/NoteForm.jsx
+++ src/components/NoteForm/NoteForm.jsx
@@ -1,51 +1,73 @@
 import s from "./NoteForm.module.css";
 import { PencilFill, Trash, TrashFill } from "react-bootstrap-icons";
 import { ButtonPrimary } from "components/ButtonPrimary/ButtonPrimary";
+import { useState } from "react";
 
-export function NoteForm({ title }) {
-    
-  const actionIcons = (
-    <>
-      <div className="col-1">
-        <PencilFill className={s.icon} />
-      </div>
-      <div className="col-1">
-        <TrashFill className={s.icon} />
-      </div>
-    </>
-  );
+export function NoteForm({ title, onClickEdit, onClickTrash, onSubmit }) {
 
-  const titleInput = (
-    <>
-      <label className="form-label">Title</label>
-      <input type="text" name="title" className="form-control" />
-    </>
-  );
+    //contient les names de chaque input
+    const [formValues, setFormValues] = useState({ title: "", content: "" });
 
-  const contentInput = (
-    <>
-      <label className="form-label">Content</label>
-      <textarea type="text" name="content" className="form-control" row="5" />
-    </>
-  );
+    function updateFormValues(e) {
+        //utilisation de spread operator pour ne pas écraser les autres valeurs
+        setFormValues({ ...formValues, [e.target.name]: e.target.value });
+        // [e.target.name] permet de récupérer le name de l'input => key de l'objet
+        // e.target.value permet de récupérer la valeur de l'input => value de l'objet
+    }
 
-  const submitButton = (
-    <div className={s.submit_btn}>
-      <ButtonPrimary>Submit</ButtonPrimary>
-    </div>
-  );
+    const actionIcons = (
+        <>
+            <div className="col-1">
+                {onClickEdit && <PencilFill onClick={onClickEdit} className={s.icon} />}
+            </div>
+            <div className="col-1">
+                {onClickTrash && <TrashFill onClick={onClickTrash} className={s.icon} />}
+            </div>
+        </>
+    );
 
-  return (
-    <div className={s.container}>
-      <div className="row justify-content-space-between">
-        <div className="col-10">
-          <h2 className="mb-3">{title}</h2>
+    const titleInput = (
+        <>
+            <label className="form-label">Title</label>
+            <input
+                onChange={updateFormValues}
+                type="text"
+                name="title"
+                className="form-control"
+            />
+        </>
+    );
+
+    const contentInput = (
+        <>
+            <label className="form-label">Content</label>
+            <textarea
+                onChange={updateFormValues}
+                type="text"
+                name="content"
+                className="form-control"
+                row="5"
+            />
+        </>
+    );
+
+    const submitButton = (
+        <div className={s.submit_btn}>
+            <ButtonPrimary onClick={() => onSubmit(formValues)} >Submit</ButtonPrimary>
         </div>
-        {actionIcons}
-      </div>
-      <div className={`mb-3 ${s.title_input_container}`}>{titleInput}</div>
-      <div className="mb-3">{contentInput}</div>
-      {submitButton}
-    </div>
-  );
+    );
+
+    return (
+        <form className={s.container}>
+            <div className="row justify-content-space-between">
+                <div className="col-10">
+                    <h2 className="mb-3">{title}</h2>
+                </div>
+                {actionIcons}
+            </div>
+            <div className={`mb-3 ${s.title_input_container}`}>{titleInput}</div>
+            <div className="mb-3">{contentInput}</div>
+            {onSubmit && submitButton}
+        </form>
+    );
 }
--- src/pages/NoteCreate/NoteCreate.jsx
+++ src/pages/NoteCreate/NoteCreate.jsx
@@ -1,10 +1,27 @@
 import { NoteForm } from 'components/NoteForm/NoteForm';
 import s from './NoteCreate.module.css';
+import { NoteAPI } from 'api/note-api';
+import { useDispatch } from 'react-redux';
+import { addNote } from 'store/note/note-slice';
+import { useNavigate } from 'react-router-dom';
 
 export function NoteCreate(props) {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    async function createNote(formValues) {
+        //ajout de la date
+       const createdNote = await NoteAPI.create({...formValues, created_at: new Date().toLocaleDateString()});
+         dispatch(addNote(createdNote));
+            navigate("/");
+    }
     return (
         <div className={s.NoteCreate}>
-        <NoteForm title="Create a note" />
+            <NoteForm
+                title="Create a note"
+                onSubmit={createNote}
+            />
         </div>
     );
-    }
+}
